71900cc68a0171122d5a359cabd7cd24eade8053
Jasonjsho <jh1827@exeter.ac.uk>
2026-01-20 15:15:56 +0000

    Completed check_higher_lower function

--- game_functions.py
+++ game_functions.py
@@ -7,7 +7,15 @@ def pick_value(poss_values):
 
 # function to be used in game_2: Higher or Lower
 def check_higher_lower(current_val, next_val, user_input):
-    pass
+    if next_val > current_val:
+        if user_input == "h":
+            return True
+        else:
+            return False 
+    elif next_val < current_val:
+        if user_input == "h":
+            return False
+        else: return True
 
 # function to be used in game_3: Hangman
 def process_guess(letter, board, word):
